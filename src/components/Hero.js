7173f5d652d7361d46c99c0eb0700211f373009b
JulietaGonella <julietagonella2001@hotmail.com>
2025-10-20 13:50:28 -0300

    primer commit

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,9 +7,11 @@ function Hero() {
     <img src={profile} alt="Julieta Gonella" /> 
     <div> 
       <h2>¡Hola! Soy Julieta Gonella 👋</h2> 
+      
       <p> Estudiante de Analista en Sistemas y Programadora Junior apasionada por el desarrollo web. 
         Me encanta crear soluciones funcionales y aprender nuevas tecnologías. 
       </p> 
+
       <a href="/cv.pdf" className="btn">Descargar CV</a>
     </div> 
   </div>
