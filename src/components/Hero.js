641ae82ea073bdbcb41558ff5366e9f3d92b45d5
JulietaGonella <julietagonella2001@hotmail.com>
2025-10-20 12:23:35 -0300

    primer commit

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,19 +1,14 @@
-function Hero() {
-  return (
-    <section className="hero">
-      <div className="hero-content">
-        <img src="../profile.jpg" alt="Julieta Gonella" /> {/* o .jpeg */}
-        <div>
-          <h2>¡Hola! Soy Julieta Gonella 👋</h2>
-          <p>
-            Estudiante de Analista en Sistemas y Programadora Junior apasionada
-            por el desarrollo web. Me encanta crear soluciones funcionales y
-            aprender nuevas tecnologías.
-          </p>
-          <a href="/cv.pdf" className="btn">Descargar CV</a>
-        </div>
-      </div>
-    </section>
-  );
-}
+jsx import profile from '../assets/profile.jpeg'; 
+function Hero() { 
+  return ( 
+  <section className="hero"> 
+  <div className="hero-content"> 
+    <img src={profile} alt="Julieta Gonella" /> 
+  <div> 
+    <h2>¡Hola! Soy Julieta Gonella 👋</h2> 
+    <p> Estudiante de Analista en Sistemas y Programadora Junior apasionada por el desarrollo web. 
+      Me encanta crear soluciones funcionales y aprender nuevas tecnologías. 
+    </p> 
+    <a href="/cv.pdf" className="btn">Descargar CV</a> </div> </div> </section> );
+} 
 export default Hero;
